fdd2f8eb198b0d93276ba45d21c45ea825865f71
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-21 18:12:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,26 @@
 class Author
   attr_accessor :name
 
-  @@post_count = 0
 
   def initialize(name)
     @name = name
-    @posts = []
   end
 
   def posts
-    @posts
+    Post.all.select { |post| post.author == self  }
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
   end
 
   def self.post_count
-    @@post_count
+    Post.all.count
   end
 
-  def add_post(post_title)
-    @posts << post_title
-    post_title.author = self
-    @@post_count += 1
-  end
-
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
 end
