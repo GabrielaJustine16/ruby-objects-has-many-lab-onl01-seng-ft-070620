f0291a75765fbd34d610f4d0d8c3238ea62fe1b3
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-21 18:11:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,25 @@
 class Artist
-  attr_accessor :name
-
-  @@song_count = 0
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
-    @songs = []
-  end
 
-  def self.song_count
-    @@song_count
   end
 
   def songs
-    @songs
+    Song.all.select {|song| song.artist == self}
   end
 
-  def add_song(song_name)
-    @songs << song_name
-    song_name.artist = self
-    @@song_count += 1
-  end
-
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song
+  def add_song(song)
     song.artist = self
-    @@song_count += 1
   end
-end 
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+end
